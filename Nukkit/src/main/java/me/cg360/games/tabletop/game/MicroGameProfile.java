1c89dcc23846f998a2f96fa03792139108357d3f
Will <16578982+CloudG360@users.noreply.github.com>
2021-03-21 18:41:52 +0000

    Implemented key support for MicroGameProfile + added registry register methods.

--- Nukkit/src/main/java/me/cg360/games/tabletop/game/MicroGameProfile.java
+++ Nukkit/src/main/java/me/cg360/games/tabletop/game/MicroGameProfile.java
@@ -1,17 +1,25 @@
 package me.cg360.games.tabletop.game;
 
 import net.cg360.nsapi.commons.data.Settings;
+import net.cg360.nsapi.commons.data.keyvalue.Key;
+import net.cg360.nsapi.commons.id.Identifier;
 
 public class MicroGameProfile<T extends MicroGameBehaviour> {
 
-    protected Settings properties;
+    protected Identifier identifier;
     protected Class<T> behaviourClass;
 
+    protected Settings properties;
 
-    public MicroGameProfile(Class<T> behaviourClass, Settings properties) {
+
+    public MicroGameProfile(Identifier identifier, Class<T> behaviourClass, Settings properties) {
+        this.identifier = identifier;
         this.behaviourClass = behaviourClass;
+
         this.properties = properties.lock();
     }
+
+
 
     public MicroGameWatchdog<T> createInstance(Settings settings) {
         try {
@@ -28,6 +36,13 @@ public class MicroGameProfile<T extends MicroGameBehaviour> {
         }
     }
 
+
+    /** @return a typed key for this game profile. */
+    public final Key<MicroGameProfile<T>> getKey() {
+        return new Key<>(identifier);
+    }
+
+    public Identifier getIdentifier() { return identifier; }
     public Class<T> getBehaviours() { return behaviourClass; }
     public Settings getProperties() { return properties.getUnlockedCopy(); }
 }
--- Nukkit/src/main/java/me/cg360/games/tabletop/game/MicroGameRegistry.java
+++ Nukkit/src/main/java/me/cg360/games/tabletop/game/MicroGameRegistry.java
@@ -1,4 +1,35 @@
 package me.cg360.games.tabletop.game;
 
+import net.cg360.nsapi.commons.data.keyvalue.Key;
+
+import java.util.HashMap;
+
 public class MicroGameRegistry {
+
+    private HashMap<String, MicroGameProfile<?>> gameProfiles;
+
+
+    public MicroGameRegistry() {
+        this.gameProfiles = new HashMap<>();
+    }
+
+
+
+    /** @return the profile's key for use in key lists. Is silent if a duplicate occurs. */
+    public <T extends MicroGameBehaviour> Key<MicroGameProfile<T>> registerProfile(MicroGameProfile<T> profile) {
+        register(profile);
+        return profile.getKey();
+    }
+
+    /** @return true if there was not a profile already registered. */
+    public boolean register(MicroGameProfile<?> profile) {
+        String key = profile.getKey().get();
+
+        if(!gameProfiles.containsKey(key)) {
+            this.gameProfiles.put(key, profile);
+            return true;
+        }
+        return false;
+    }
+
 }
