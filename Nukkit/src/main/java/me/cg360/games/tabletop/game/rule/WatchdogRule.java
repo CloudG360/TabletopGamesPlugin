f63bb69372a7c1dbd6bd7495c42b5e8015cb53e5
Will <16578982+CloudG360@users.noreply.github.com>
2021-03-21 19:52:57 +0000

    Add base WatchdogRule

---
+++ Nukkit/src/main/java/me/cg360/games/tabletop/game/rule/WatchdogRule.java
@@ -0,0 +1,21 @@
+package me.cg360.games.tabletop.game.rule;
+
+import me.cg360.games.tabletop.game.MicroGameWatchdog;
+
+/**
+ * Handles specific checks which would typically be watched by a
+ * minigame API.
+ */
+public abstract class WatchdogRule {
+
+    public MicroGameWatchdog<?> watchdog;
+
+    public WatchdogRule(MicroGameWatchdog<?> watchdog) {
+        this.watchdog = watchdog;
+    }
+
+    protected abstract void onStartWatchdog();
+    protected abstract void onStopWatchdog();
+
+
+}
