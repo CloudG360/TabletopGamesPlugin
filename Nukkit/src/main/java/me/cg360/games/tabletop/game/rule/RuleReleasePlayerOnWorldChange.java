7b79f861a39bc351dd4e3eba450c9c4afbaebad3
Will <16578982+CloudG360@users.noreply.github.com>
2021-03-21 21:37:19 +0000

    Add RuleDetectWorldChange

---
+++ Nukkit/src/main/java/me/cg360/games/tabletop/game/rule/RuleReleasePlayerOnWorldChange.java
@@ -0,0 +1,44 @@
+package me.cg360.games.tabletop.game.rule;
+
+import cn.nukkit.Player;
+import cn.nukkit.event.EventHandler;
+import cn.nukkit.event.HandlerList;
+import cn.nukkit.event.Listener;
+import cn.nukkit.event.player.PlayerTeleportEvent;
+import me.cg360.games.tabletop.TabletopGamesNukkit;
+import me.cg360.games.tabletop.game.MicroGameWatchdog;
+import me.cg360.games.tabletop.game.WatchdogRule;
+
+import java.util.HashMap;
+
+public class RuleReleasePlayerOnWorldChange extends WatchdogRule implements Listener {
+
+    public RuleReleasePlayerOnWorldChange(MicroGameWatchdog<?> watchdog) {
+        super(watchdog);
+    }
+
+    @Override
+    protected void onStartWatchdog() {
+        TabletopGamesNukkit.get().getServer().getPluginManager().registerEvents(this, TabletopGamesNukkit.get());
+    }
+
+    @Override
+    protected void onStopWatchdog() {
+        HandlerList.unregisterAll(this);
+    }
+
+    @EventHandler
+    public void onChangeDaWorld(PlayerTeleportEvent event) {
+
+        if(event.getFrom().getLevel() != event.getTo().getLevel()) { // Check for level change.
+
+            if(MicroGameWatchdog.getPlayerWatchdogs().containsKey(event.getPlayer())) { // Check it's a game player.
+                HashMap<Player, MicroGameWatchdog<?>> w = MicroGameWatchdog.getPlayerWatchdogs();
+
+                if(w.get(event.getPlayer()) == watchdog) {
+                    watchdog.releasePlayer(event.getPlayer());
+                }
+            }
+        }
+    }
+}
