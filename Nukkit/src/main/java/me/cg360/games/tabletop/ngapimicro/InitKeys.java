48a794753bd0efcbb38c6ee573c173b3f8dc80ed
Will <16578982+CloudG360@users.noreply.github.com>
2021-03-22 16:59:42 +0000

    Add initial init keys

---
+++ Nukkit/src/main/java/me/cg360/games/tabletop/ngapimicro/InitKeys.java
@@ -0,0 +1,17 @@
+package me.cg360.games.tabletop.ngapimicro;
+
+import cn.nukkit.Player;
+import cn.nukkit.level.Location;
+import net.cg360.nsapi.commons.data.keyvalue.Key;
+
+public class InitKeys {
+
+    public static final Key<Player> HOST_PLAYER = new Key<>("host_player");
+
+    // Where was the game started?
+    public static final Key<Location> ORIGIN = new Key<>("origin");
+
+    // Where was the game started?
+    public static final Key<Location> INVITE_RADIUS = new Key<>("invite_radius");
+
+}
