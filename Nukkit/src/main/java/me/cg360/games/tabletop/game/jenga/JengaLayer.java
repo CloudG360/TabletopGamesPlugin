ea87a25034e015facc23adda9e781f7cbb236d21
Will <16578982+CloudG360@users.noreply.github.com>
2021-03-26 17:39:24 +0000

    JengaLayer now stores NBT in EntityJengaBlock of where in the tower it is.

--- Nukkit/src/main/java/me/cg360/games/tabletop/game/jenga/JengaLayer.java
+++ Nukkit/src/main/java/me/cg360/games/tabletop/game/jenga/JengaLayer.java
@@ -10,15 +10,20 @@ import me.cg360.games.tabletop.game.jenga.entity.EntityJengaBlock;
 import net.cg360.nsapi.commons.Check;
 
 import java.util.Optional;
+import java.util.UUID;
 
 public class JengaLayer {
 
+    public static final String NBT_COMPOUND_TOWER = "tower_data";
+
+    public static final String NBT_TOWER_UUID = "tower_uuid";
     public static final String NBT_LAYERS_BELOW_COUNT = "layers_below";
-    public static final String NBT_POS_IN_LAYER = "layers_below";
+    public static final String NBT_POS_IN_LAYER = "layer_pos";
     public static final String NBT_LAYER_ALTERNATE_AXIS = "layer_alternate_axis";
 
 
     protected JengaLayer layerBelow;
+    protected UUID towerUUID;
     protected int layersBelowCount;
 
     protected Location layerOrigin;
@@ -35,12 +40,13 @@ public class JengaLayer {
     public JengaLayer(JengaLayer belowInStack) {
         this(belowInStack.getLayerOrigin().getLocation(), belowInStack.getScale(), !belowInStack.isAxisAlternate()); // Retain scale, flip alternate axis value.
         this.layerBelow = belowInStack;
+        this.towerUUID = belowInStack.getTowerUUID(); // Copy UUID, it's now part of the tower :D
         this.layersBelowCount = belowInStack.getLayersBelowCount() + 1;
         this.layerOrigin = layerOrigin.add(0, scale, 0); //Update location, stack y up by 1.
     }
 
     /**
-     * Creates an empty JengaBlock layer.
+     * Creates an empty JengaBlock layer. This is the root of a tower.
      * @param layerOrigin the central position of the layer.
      * @param scale the scale of the blocks (and positions) of the layer)
      * @param isAxisAlternate True if the blocks are placed up the X axis. False if they're placed up the Z axis.
@@ -50,6 +56,7 @@ public class JengaLayer {
         Check.nullParam(layerOrigin.getLevel(), "layerOrigin.level");
 
         this.layerBelow = null;
+        this.towerUUID = UUID.randomUUID();
         this.layersBelowCount = 0;
 
         this.layerOrigin = layerOrigin;
@@ -87,7 +94,7 @@ public class JengaLayer {
                 );
             }
 
-            this.left = spawnBlock(loc);
+            this.left = spawnBlock(loc, 0);
             return true;
         }
         return false;
@@ -100,7 +107,7 @@ public class JengaLayer {
             // Spawns in the center anyway, no need for an if statement.
             Location loc = new Location(layerOrigin.getX(), layerOrigin.getY(), layerOrigin.getZ(), layerOrigin.getLevel());
 
-            this.center = spawnBlock(loc);
+            this.center = spawnBlock(loc, 1);
             return true;
         }
         return false;
@@ -129,7 +136,7 @@ public class JengaLayer {
                 );
             }
 
-            this.right = spawnBlock(loc);
+            this.right = spawnBlock(loc, 2);
             return true;
         }
         return false;
@@ -142,8 +149,13 @@ public class JengaLayer {
         spawnRight();
     }
 
-    /** @return the id of the spawned block.*/
-    protected EntityJengaBlock spawnBlock(Location position) {
+    /**
+     * Spawns an entity for a Jenga Block.
+     * @param position the position in the world of the EntityJengaBlock
+     * @param positionWithinLayer 0 = left; 1 = center; 2 = right;
+     * @return the id of the spawned block.
+     */
+    protected EntityJengaBlock spawnBlock(Location position, int positionWithinLayer) {
 
         CompoundTag nbt = new CompoundTag()
                 .putList(new ListTag<>("Pos")
@@ -158,7 +170,14 @@ public class JengaLayer {
                         .add(new FloatTag("", isAxisAlternate ? 90f : 0f))
                         .add(new FloatTag("", 0f)))
                 .putBoolean("npc", true)
-                .putFloat("scale", scale);
+                .putFloat("scale", scale)
+                .putCompound(NBT_COMPOUND_TOWER, new CompoundTag()
+                        .putString(NBT_TOWER_UUID, towerUUID.toString())
+                        .putInt(NBT_POS_IN_LAYER, positionWithinLayer)
+                        .putInt(NBT_LAYERS_BELOW_COUNT, layersBelowCount)
+                        .putBoolean(NBT_LAYER_ALTERNATE_AXIS, isAxisAlternate)
+                );
+
         nbt.putBoolean("ishuman", true);
 
         FullChunk chunk = position.getLevel().getChunk((int) Math.floor(position.getX() / 16), (int) Math.floor(position.getZ() / 16), true);
@@ -177,6 +196,8 @@ public class JengaLayer {
 
     /** @return the layer below this layer in the Jenga stack. Empty means it is the bottom of the tower.*/
     public Optional<JengaLayer> getLayerBelow() { return Optional.ofNullable(layerBelow); }
+    /** @return the unique id of the tower this layer is part of. */
+    public UUID getTowerUUID() { return towerUUID; }
     /** @return the amount of layers below this layer. */
     public int getLayersBelowCount() { return layersBelowCount; }
 
