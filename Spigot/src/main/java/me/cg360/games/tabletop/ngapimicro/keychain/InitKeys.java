c8ede8c4b8669a037441eb494bd01b1d0116a4e5
Will <16578982+CloudG360@users.noreply.github.com>
2021-03-27 10:56:24 +0000

    Parity with new InitKeys from Nukkit (Spigot)

--- Spigot/src/main/java/me/cg360/games/tabletop/ngapimicro/keychain/InitKeys.java
+++ Spigot/src/main/java/me/cg360/games/tabletop/ngapimicro/keychain/InitKeys.java
@@ -12,10 +12,16 @@ public class InitKeys {
     // Where was the game started?
     public static final Key<Location> ORIGIN = new Key<>("origin");
 
+    // How far should the radius where players are part of the game reach
+    public static final Key<Double> PLAY_AREA_RADIUS = new Key<>("play_area_radius");
+
     // How far around the origin should invites be sent to.
     public static final Key<Double> INVITE_RADIUS = new Key<>("invite_radius");
 
     // How long (in ticks) should "yes" responses be accepted for.
     public static final Key<Integer> INITIAL_INVITE_LENGTH = new Key<>("init_invite_length");
+
+    // Are debug messages and behaviours enabled in the game?
+    public static final Key<Boolean> DEBUG_MODE_ENABLED = new Key<>("is_debug_enabled");
 
 }
